12122e203772ee6d362cdc750ba5304094ce1f20
Pol Gubau Amores <gubaupol@gmail.com>
2025-08-30 20:11:50 +0200

    Add session handling and sign-in page redirection
    Implement session management in the Favs component to ensure users are signed in before accessing the favorites page. If the user is not authenticated, they will be redirected to the PleaseSignInPage for login.
    
    Issue: MW-0000
    Type: feature

--- app/routes/favs.tsx
+++ app/routes/favs.tsx
@@ -1,9 +1,16 @@
 import FavsPage from "src/global/pages/Favs";
+import { PleaseSignInPage } from "~/shared/components/pages/PleaseSignInPage";
+import { useSession } from "~/shared/context/session-context";
 
 export function meta() {
 	return [{ title: "Flatmatch" }, { content: "Welcome to Flatmatch!", name: "description" }];
 }
 
 export default function Favs() {
+	const { session } = useSession();
+
+	if (!session) {
+		return <PleaseSignInPage />;
+	}
 	return <FavsPage />;
 }
